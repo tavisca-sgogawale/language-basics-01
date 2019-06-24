30f0cdb7f8a76480e523b21a0a39a9908e148f78
tavisca-sgogawale <51969135+tavisca-sgogawale@users.noreply.github.com>
2019-06-25 01:08:28 +0530

    Added my code

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Text.RegularExpressions;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
@@ -22,8 +23,52 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            try
+            {
+                equation = equation.Replace('*', '=');
+                string[] eqArr = Regex.Split(equation, "=");
+                if (eqArr[0].Contains("?"))
+                {
+                    return findAns(eqArr[2], eqArr[1], eqArr[0], "/");
+                }
+                else if (eqArr[1].Contains("?"))
+                {
+                    return findAns(eqArr[2], eqArr[0], eqArr[1], "/");
+                }
+                else
+                {
+                    return findAns(eqArr[0], eqArr[1], eqArr[2], "*");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write(e);
+                throw new NotImplementedException();
+            }
+        }
+
+        public static int findAns(string equation1, string equation2, string equation3, string operation)
+        {
+            int result = 0;
+            switch (operation)
+            {
+                case "/":
+                    result = int.Parse(equation1) / int.Parse(equation2);
+                    if (int.Parse(equation1) % int.Parse(equation2) != 0 || equation3.Contains(result.ToString()))
+                    {
+                        return -1;
+                    }
+                    break;
+
+                case "*":
+                    result = int.Parse(equation1) * int.Parse(equation2);
+                    break;
+
+                default: return -1;
+
+            }
+            char result_string = ((int)result).ToString()[equation3.IndexOf('?')];
+            return int.Parse(result_string.ToString());
         }
     }
 }
