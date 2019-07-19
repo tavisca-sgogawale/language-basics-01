a661f7b3350bbfb445b230a6937cf404015bb599
Shubham Gogawale <sgogawale@affinionds.com>
2019-07-19 11:53:43 -0700

    updated naming conventios

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -16,10 +16,10 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
             //If user wants to test new data or equation
             Console.WriteLine("Enter Equation in the Form of 'A*B=C' With One '?' at Any Numeric Position");
-            string eqn = Console.ReadLine();
+            string equation = Console.ReadLine();
             Console.WriteLine("Enter Result Value of '?' in The Above Equation");
             int num = Convert.ToInt32(Console.ReadLine());
-            Test(eqn, num);
+            Test(equation, num);
             
             Console.ReadKey(true);
         }
@@ -35,42 +35,42 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             try
             {
                 equation = equation.Replace('*', '=');
-                string[] eqArr = Regex.Split(equation, "=");
-                string a = eqArr[0];
-                string b = eqArr[1];
-                string c = eqArr[2];
-                int result = 0;
-                if (a.Contains("?"))
+                string[] SplitedEquation = Regex.Split(equation, "=");
+                string Operand1 = SplitedEquation[0];
+                string Operand2 = SplitedEquation[1];
+                string RHS_Result = SplitedEquation[2]; //RightHandSight resut
+                int Result = 0;
+                if (Operand1.Contains("?"))
                 {
-                    result = int.Parse(c) / int.Parse(b);
-                    if (int.Parse(c) % int.Parse(b) !=0 || a.Length != result.ToString().Length)
+                    Result = int.Parse(RHS_Result) / int.Parse(Operand2);
+                    if (int.Parse(RHS_Result) % int.Parse(Operand2) !=0 || Operand1.Length != Result.ToString().Length)
                     {
                         return -1;
                     }
                     else
                     {
-                        char result_string = ((int)result).ToString()[a.IndexOf('?')];
+                        char result_string = ((int)Result).ToString()[Operand1.IndexOf('?')];
                         return int.Parse(result_string.ToString());
                     }
                  }
-                else if (b.Contains("?"))
+                else if (Operand2.Contains("?"))
                 {
-                    result = int.Parse(c) / int.Parse(a);
-                    if (int.Parse(c) % int.Parse(a) != 0 || b.Length!=result.ToString().Length)
+                    Result = int.Parse(RHS_Result) / int.Parse(Operand1);
+                    if (int.Parse(RHS_Result) % int.Parse(Operand1) != 0 || Operand2.Length!=Result.ToString().Length)
                     {
                         return -1;
                     }
                     else
                     {
-                        char result_string = ((int)result).ToString()[b.IndexOf('?')];
-                        return int.Parse(result_string.ToString());
+                        char DigitToFind = ((int)Result).ToString()[Operand2.IndexOf('?')];
+                        return int.Parse(DigitToFind.ToString());
                     }
                 }
                 else
                 {
-                    result = int.Parse(a) * int.Parse(b);
-                    char result_string = ((int)result).ToString()[c.IndexOf('?')];
-                    return int.Parse(result_string.ToString());
+                    Result = int.Parse(Operand1) * int.Parse(Operand2);
+                    char DigitToFind = ((int)Result).ToString()[RHS_Result.IndexOf('?')];
+                    return int.Parse(DigitToFind.ToString());
                 }
             }
             catch (Exception e)
